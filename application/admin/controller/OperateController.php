b2822b7b02a48dea638b1774f7c9aa8e7a5ea034
lxl <2427396682@qq.com>
2021-01-27 18:45:27 +0800

    运营中心3

--- application/admin/controller/OperateController.php
+++ application/admin/controller/OperateController.php
@@ -4,6 +4,7 @@
 namespace app\admin\controller;
 
 
+use app\admin\model\AnalysisFormFieldValueModel;
 use app\admin\model\AnalysisFormModel;
 use app\admin\model\AnalysisNameModel;
 use think\Db;
@@ -14,17 +15,36 @@ class OperateController extends CommonController
     public function operate_list()
     {
         //查询自有表单字段的已填写列表
+        $platform_name  =   Db::name('analysis_form')
+                            ->where(['note_taker_id'=>$this->cookie_admin()['id']])
+                            ->field('platform_name_id,platform_name,note_taker_name')
+                            ->group('platform_name')
+                            ->select();
 
-        return view();
+        foreach ($platform_name as $key=>$item)
+        {
+            $where  =   [
+                "platform_name"     =>  $item['platform_name'],
+                "platform_name_id"  =>  $item['platform_name_id'],
+                "note_taker_name"   =>  $item['note_taker_name'],
+            ];
+            $platform_name[$key]['model']   =   $this->assoc_unique(Db::name('analysis_form')->where($where)->select(),'analysis_name');
+        }
+        return view('',['platform'=>$platform_name]);
     }
 
-
-    //表单数据点分配，
-    public function operate_form_field()
-    {
-        //查询已分配的
-
-        return view('');
+    //二维数组去重
+    function assoc_unique($arr, $key) {
+        $tmp_arr = array();
+        foreach ($arr as $k => $v) {
+            if (in_array($v[$key], $tmp_arr)) {//搜索$v[$key]是否在$tmp_arr数组中存在，若存在返回true
+                unset($arr[$k]);
+            } else {
+                $tmp_arr[] = $v[$key];
+            }
+        }
+        sort($arr); //sort函数对数组进行排序
+        return $arr;
     }
 
     //添加分析模块页面
@@ -34,106 +54,59 @@ class OperateController extends CommonController
         $platform_name  =   Db::name('platform_name')->select();
         //取管理员
         $admin          =   Db::name('admin')->select();
-        return view('',['platform_name'=>$platform_name,'admin'=>$admin]);
+        //分析模块名
+        $analysis_model =   Db::name('analysis_name')->select();
+        return view('',['platform_name'=>$platform_name,'admin'=>$admin,'analysis_model'=>$analysis_model]);
     }
 
-    //添加分析模块以及对于字段和字段记录人
-    public function analysis_model_add_do()
+    //分配页面，根据所选平台查询分析模块
+    public function platformFindAnalysis()
+    {
+        $sql        =   new AnalysisNameModel();
+        $analysis   =   $sql->where(input())->select();
+        echo json_encode(['code'=>200,'msg'=>'成功!','result'=>$analysis]);
+    }
+
+    //运营平台的模块管理
+    public function operate_administration()
     {
         $data       =   input();
-        $sql        =   new AnalysisNameModel();
-        $where      =   ['platform_id'=>$data['platform_id'],'paltform_name'=>$data['paltform_name'],'analysis_name'=>$data['analysis_name']];
-        $analysis   =   $sql->where($where)->find();
-        if($analysis)
-        {
-            //如果该平台下，该模块已存在，则直接添加对应表单字段
-            $analysisFormSql   =   new AnalysisFormModel();
-            $insert =   [
-                            'analysis_name_id'  =>  $analysis['analysis_name_id'],
-                            'analysis_name'     =>  $data['analysis_name'],
-                            'form_field'        =>  $data['form_field'],
-                            'create_time'       =>  date("Y-m-d H:i:s",time()),
-                            'create_by'         =>  $this->cookie_admin()['name'],
-                            'note_taker_id'     =>  $data['people_id'],
-                            'note_taker_id'     =>  $data['people_name'],
-                        ];
-            try{
-                $result =   $analysisFormSql->insert($insert);
-                if($result)
-                {
-                    echo json_encode(['code'=>200,'msg'=>'添加成功！']);
-                }
-            }catch(\Exception $e){
-                echo json_encode(['code'=>500,'msg'=>$e->getMessage()]);
-            }
-        }else{
-            //如果改平台下，不存在，则添加模块，然后添加模块对应字段
-            // 启动事务
-            Db::startTrans();
-            try {
-                $insert1 =   [
-                    'analysis_name'     =>  $data['analysis_name'],
-                    'create_time'       =>  date("Y-m-d H:i:s",time()),
-                    'create_by'         =>  $this->cookie_admin()['name'],
-                    'platfoem_name_id'  =>  $data['platform_id'],
-                    'platform_name'     =>  $data['platform_name'],
-                ];
-                $analysis_name_id   =   Db::table('analysis_name')->insertGetId($insert1);
-                $insert2 =   [
-                    'analysis_name_id'  =>  $analysis_name_id,
-                    'analysis_name'     =>  $data['analysis_name'],
-                    'form_field'        =>  $data['form_field'],
-                    'create_time'       =>  date("Y-m-d H:i:s",time()),
-                    'create_by'         =>  $this->cookie_admin()['name'],
-                    'note_taker_id'     =>  $data['people_id'],
-                    'note_taker_id'     =>  $data['people_name'],
-                ];
-                Db::table('analysis_form')->insert($insert2);
-                // 提交事务
-                Db::commit();
-            } catch (\Exception $e) {
-                // 回滚事务
-                Db::rollback();
-                echo json_encode(['code'=>500,'msg'=>$e->getMessage()]);
-            }
-            echo json_encode(['code'=>200,'msg'=>'添加成功！']);
-        }
+        $admin_id   =   $this->cookie_admin()['id'];
+        //查询已记录的数据展示
+        $sql        =   new AnalysisFormModel();
+        $where      =   [
+            'note_taker_id'=>$admin_id,
+            'platform_name_id'=>input('platform_name_id'),
+
+        ];
+        $analysis_form_data =   $sql->where($where)->select();
 
     }
 
+    //运营平台的模块管理
+    public function analysis_model_admin()
+    {
+        $data   =   input();
+        $data['note_taker_id']  =   $this->cookie_admin()['id'];
+        $sql    =   new AnalysisFormFieldValueModel();
+        $result =   $sql->where($data)->order('create_time desc')->group('create_time')->select()->toArray();
+        return view('',['result'=>$result]);
+    }
 
 
-    //分析模块下添加表字段，并指定记录人
-    public function analysis_form_field()
+    //运营平台的模块字段数据添加页面
+    public function analysis_model_data_add()
+    {
+        //查询该管理员在改平台以及该模块下，所需要填写的字段。
+
+        return view();
+    }
+    //运营平台的模块字段数据添加提交
+    public function analysis_model_data_add_do()
     {
 
     }
 
 
-    //子屏表单数据点添加页面
-    public function operate_form_field_add()
-    {
 
-    }
-
-
-    //子屏表单数据点添加
-    public function operate_form_field_add_do()
-    {
-
-    }
-
-
-    //子屏表单数据添加页面
-    public function operate_form_add()
-    {
-
-    }
-
-
-    //子屏表单数据添加
-    public function operate_form_add_do()
-    {
-
-    }
 }
